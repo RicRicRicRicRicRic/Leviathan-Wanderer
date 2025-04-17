309956fba4d0451e1dd32b837c2a5a19520082b8
Estremadura, Ric Michael <ricmichaele@gmail.com>
2025-04-17 16:30:26 +0800

    bug fixes, and rapier removal

--- leviathan-wanderer/scripts/enemy-scripts/slug/slug.gd
+++ leviathan-wanderer/scripts/enemy-scripts/slug/slug.gd
@@ -1,53 +1,43 @@
 #slug.gd
 extends "res://scripts/Optimization/Interpolate.gd"
 
-const SPEED: float = 380.0           
-const GRAVITY: float = 980.0         
+const SPEED: float = 380.0
+const GRAVITY: float = 980.0
 
 @export var max_health: int = 475
 var health: int = max_health
 
-@onready var visuals: Node2D = $Node2D
-@onready var slime_main: AnimatedSprite2D = visuals.get_node("AnimatedSprite2D") as AnimatedSprite2D
-@onready var raycast_wall: RayCast2D = visuals.get_node("RayCast2D_wall") as RayCast2D
-@onready var raycast_fall: RayCast2D = visuals.get_node("RayCast2D_height") as RayCast2D
-@onready var damage_area: Area2D = visuals.get_node("DamageArea") as Area2D
-@onready var head_collision: CollisionShape2D = $CollisionShape2D_head
-@onready var body_collision: CollisionShape2D = $CollisionShape2D_body
-@onready var Time_before_turn: Timer = $Timer
+@onready var visuals         := $Node2D
+@onready var trail_area      := visuals.get_node("TrailArea")          as Node
+@onready var slime_main      := visuals.get_node("AnimatedSprite2D")    as AnimatedSprite2D
+@onready var raycast_wall    := visuals.get_node("RayCast2D_wall")     as RayCast2D
+@onready var raycast_fall    := visuals.get_node("RayCast2D_height")   as RayCast2D
+@onready var damage_area     := visuals.get_node("DamageArea")         as Area2D
+@onready var collision_shape := $CollisionPolygon2D_collider             as CollisionPolygon2D
+@onready var turn_timer      := $Timer                                 as Timer
 
-var head_original_x: float
-var head_original_rot_deg: float
-var body_original_x: float
-var direction: int = -1
-var was_colliding: bool = false
-var should_flip: bool = false
-var player_in_contact: Node = null
+var collision_original_x: float
+var direction: int       = -1
+var was_colliding: bool  = false
+var should_flip: bool    = false
 
 func _ready() -> void:
+	interp_visuals = visuals
+	previous_position = global_position
 	add_to_group("enemy")
+
+	raycast_wall.enabled = true
+	raycast_fall.enabled = true
+	raycast_wall.add_exception(self)
+	raycast_fall.add_exception(self)
 	var player_node = get_tree().get_first_node_in_group("player")
 	if player_node:
 		raycast_wall.add_exception(player_node)
-	interp_visuals = visuals
-	previous_position = global_position
-	raycast_wall.enabled = true
-	raycast_fall.enabled = true
-	head_original_x = head_collision.position.x
-	head_original_rot_deg = rad_to_deg(head_collision.rotation)
-	body_original_x = body_collision.position.x
-	damage_area.body_entered.connect(_on_DamageArea_body_entered)
-	Time_before_turn.timeout.connect(_on_Timer_timeout)
+		raycast_fall.add_exception(player_node)
 
-func update_collision_shapes() -> void:
-	if direction == 1:
-		head_collision.position.x = -head_original_x
-		head_collision.rotation = 0.0  
-		body_collision.position.x = -body_original_x
-	else:
-		head_collision.position.x = head_original_x
-		head_collision.rotation = 0.0
-		body_collision.position.x = body_original_x
+	collision_original_x = collision_shape.position.x
+	damage_area.body_entered.connect(_on_DamageArea_body_entered)
+	turn_timer.timeout.connect(_on_Timer_timeout)
 
 func _physics_process(delta: float) -> void:
 	previous_position = global_position
@@ -55,39 +45,49 @@ func _physics_process(delta: float) -> void:
 		velocity.y += GRAVITY * delta
 	else:
 		velocity.y = 0.0
+
 	raycast_fall.force_raycast_update()
 	raycast_wall.force_raycast_update()
-	var wall_detected: bool = raycast_wall.is_colliding()
-	var fall_detected: bool = not raycast_fall.is_colliding()
-	var should_turn: bool = (wall_detected or fall_detected)
 
-	if should_turn and not was_colliding and Time_before_turn.is_stopped():
-		Time_before_turn.start()
+	var wall_hit = raycast_wall.is_colliding()
+	var drop     = not raycast_fall.is_colliding()
+	var should_turn_now = wall_hit or drop
+
+	if should_turn_now and not was_colliding and turn_timer.is_stopped():
+		turn_timer.start()
 		should_flip = true
-		velocity.x = 0.0  
+		velocity.x = 0
 		slime_main.play("iddle")
-	elif not should_turn:
-		Time_before_turn.stop()
+	elif not should_turn_now:
+		turn_timer.stop()
 		should_flip = false
 
-	if Time_before_turn.is_stopped():
+	if turn_timer.is_stopped():
 		velocity.x = SPEED * direction
-		visuals.scale.x = 1.0 if direction == -1 else -1.0
-		if not slime_main.is_playing() or slime_main.animation != "walk":
+		if direction == -1:
+			visuals.scale.x = 1.0
+			collision_shape.scale.x = 1.0
+			collision_shape.position.x = collision_original_x
+		else:
+			visuals.scale.x = -1.0
+			collision_shape.scale.x = -1.0
+			collision_shape.position.x = -collision_original_x
+		if slime_main.animation != "walk" or not slime_main.is_playing():
 			slime_main.play("walk")
-	was_colliding = should_turn
+
+	was_colliding = should_turn_now
 	move_and_slide()
-	update_collision_shapes()
 
 func _on_Timer_timeout() -> void:
 	if should_flip:
 		direction *= -1
 		should_flip = false
+		trail_area.call("reset_polygons")
 
 func _on_DamageArea_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
-		var knock_dir: Vector2 = Vector2.ZERO
-		var diff: Vector2 = body.global_position - global_position
+		var diff = body.global_position - global_position
+		var knock_dir: Vector2
 		if diff.length() > 0:
 			knock_dir = diff.normalized()
 		else:
--- leviathan-wanderer/scripts/enemy-scripts/slug/slug_trail.gd
+++ leviathan-wanderer/scripts/enemy-scripts/slug/slug_trail.gd
@@ -2,44 +2,55 @@
 #script of line2d in slug.tscn
 extends Line2D
 
-const MIN_TRAIL_LENGTH = 135.0
-const MIN_DISTANCE_BETWEEN_POINTS = 1.0
-const SHRINK_RATE_NORMAL = 100
-const SHRINK_RATE_MAX = 125    
-const MAX_POINTS = 500         
+const MIN_TRAIL_LENGTH: float = 135.0
+const MIN_DISTANCE_BETWEEN_POINTS: float = 1.0
+const SHRINK_RATE_NORMAL: float = 100
+const SHRINK_RATE_MAX: float = 125
+const MAX_POINTS: float = 475
 
-var time_accumulator := 0.0
+var time_accumulator: float = 0.0
+var cached_total_length: float = 0.0
 
 func _ready() -> void:
 	clear_points()
+	cached_total_length = 0.0
 
 func _process(delta: float) -> void:
-	var trail_area := get_parent()
-	if not trail_area:
+	var trail_area: Node2D = get_parent() as Node2D
+	if trail_area == null:
 		return
 
-	var current_pos = trail_area.global_position
-	var last_point: Vector2 = points[points.size() - 1] if points.size() > 0 else Vector2.INF
+	var current_pos: Vector2 = trail_area.global_position
 
-	if current_pos.distance_to(last_point) > MIN_DISTANCE_BETWEEN_POINTS:
+	if points.size() > 0:
+		var last_point: Vector2 = points[points.size() - 1]
+		var distance: float = current_pos.distance_to(last_point)
+		if distance > MIN_DISTANCE_BETWEEN_POINTS:
+			add_point(current_pos)
+			cached_total_length += distance
+	else:
 		add_point(current_pos)
-	var shrink_rate = SHRINK_RATE_NORMAL
-	if points.size() > MAX_POINTS:
-		shrink_rate = SHRINK_RATE_MAX
+
+	var shrink_rate: float = SHRINK_RATE_MAX if points.size() > MAX_POINTS else SHRINK_RATE_NORMAL
 	time_accumulator += delta
-	var points_to_remove = int(shrink_rate * time_accumulator)
-	for i in range(points_to_remove):
-		if points.size() > 2 and get_total_length() > MIN_TRAIL_LENGTH:
-			remove_point(0)
-		else:
-			break
+	var points_to_remove: int = int(shrink_rate * time_accumulator)
+
 	if points_to_remove > 0:
 		time_accumulator = 0.0
+
+		var removed_count: int = 0
+		while removed_count < points_to_remove and points.size() > 2 and cached_total_length > MIN_TRAIL_LENGTH:
+			if points.size() >= 2:
+				cached_total_length -= points[0].distance_to(points[1])
+				remove_point(0)
+				removed_count += 1
+			else:
+				break
+
 	while points.size() > MAX_POINTS:
+		if points.size() >= 2:
+			cached_total_length -= points[0].distance_to(points[1])
 		remove_point(0)
 
 func get_total_length() -> float:
-	var length := 0.0
-	for i in range(1, points.size()):
-		length += points[i - 1].distance_to(points[i])
-	return length
+	return cached_total_length
